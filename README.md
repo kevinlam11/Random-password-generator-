a0542f11a4f53685e3bc904f068fea1286a5182c
kevnviek303@yahoo.com <kevnivek303@yahoo.com>
2022-02-28 13:22:41 -0500

    Updated js and added Read me

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Random-password-generator-
+
+## Password Generator
+
+[Random-Password-Generator-Link]()
+
+## Description
+
+This is a random password generator. I made a website that can generate a password using random numbers,special letters, uppercase letters and regular letters.
+
+## Screenshot
--- script.js
+++ script.js
@@ -1,13 +1,76 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var count = Number(
+    prompt(
+      "Please enter the amount of characters you would like in your password."
+    )
+  );
+  console.log(count);
+
+  if (count < 8 || count > 128 || !count) {
+    alert("Pick a number between 8 and 128!.");
+    alert("Type a number.");
+    return "";
+  }
+
+  // options for password
+  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXY";
+  var numbers = "1234567890";
+  var special = "!@#$%^&*()<>?:{}+|";
+
+  var pool = letters;
+
+  // Adds uppercase letters into my password
+  if (confirm("Do you want uppercase letters in your password?")) {
+    pool += uppercase;
+  }
+
+  // Adds special characters into my password
+  if (confirm("Do you want special characters in your password?")) {
+    pool += special;
+  }
+
+  // Adds numbers into my password
+  if (confirm("Do you want numbers in your password?")) {
+    pool += numbers;
+  }
+
+  var genPassword = "";
+
+  for (var i = 0; i < count; i++) {
+    var random = Math.floor(Math.random() * letters.length);
+    genPassword += letters[random];
+  }
+
+  for (var i = 0; i < count; i++) {
+    var random = Math.floor(Math.random() * numbers.length);
+    genPassword += numbers[random];
+  }
+
+  for (var i = 0; i < count; i++) {
+    var random = Math.floor(Math.random() * uppercase.length);
+    genPassword += uppercase[random];
+  }
+
+  for (var i = 0; i < count; i++) {
+    var random = Math.floor(Math.random() * special.length);
+    genPassword += special[random];
+  }
+
+  console.log(genPassword);
+
+  return genPassword;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
